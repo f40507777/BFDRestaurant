ebafc8fd74d43450a5aa42dfcc2ec62ec3bab1da
finnho <finnho@truetel.com>
2018-08-11 16:49:01 +0800

    Initial Commit

---
+++ BFDRestaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BFDRestaurant
+//
+//  Created by Finn on 2018/8/11.
+//  Copyright © 2018年 Finn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
